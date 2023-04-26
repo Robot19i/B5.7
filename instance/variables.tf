97528144bc405dfd5321f04f1528dbe4dadc6e2f
Robot19i <129221214+Robot19i@users.noreply.github.com>
2023-04-26 23:26:21 +0300

    Add files via upload

---
+++ instance/variables.tf
@@ -0,0 +1,18 @@
+variable "instance_family_image" {
+  description = "Instance image"
+  type        = string
+  default     = "lamp"
+}
+
+variable "vpc_subnet_id" {
+  description = "VPC subnet network id"
+  type        = string
+}
+
+variable "vpc_zone_id" {
+  description = "zone_id"
+  type = string
+}
+
+
+
